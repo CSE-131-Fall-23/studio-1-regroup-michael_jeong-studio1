090619e905b4817b6a58513bc172fe937171ca70
Jayden.Michael <Jayden.Michael@JAYDENMICHAEL.kcd.local>
2023-09-07 11:19:07 -0500

    Stuff and things.

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,11 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		boolean isOrdered = true;
+		isOrdered = ((x>y && x>z && y>z) || (x<y && y<z && x<z));
+		
+		System.out.println(isOrdered);
 
 	}
 
